6f22e730f3ae75365a712d60241151725fc0480e
Gabriella Adam <gabriellavadam@gmail.com>
2022-10-21 23:58:37 -0400

    update

--- app/src/main/java/com/example/recipesapp/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/recipesapp/ui/fragments/recipes/RecipesFragment.kt
@@ -5,23 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recipesapp.MainViewModel
 import com.example.recipesapp.R
+import com.example.recipesapp.adapters.RecipesAdapter
+import com.example.recipesapp.databinding.FragmentRecipesBinding
+import com.example.recipesapp.util.Constants.Companion.API_KEY
+import com.example.recipesapp.util.NetworkResult
 
 
 class RecipesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var mainViewModel: MainViewModel
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+    private val mAdapter by lazy { RecipesAdapter() }
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        setupRecyclerView()
 
+        return binding.root
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
+            when(response){
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let { mAdapter.setData(it) }
+
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmerEffect()
+                }
+            }
+        }
+    }
+
+    private fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries["number"] = "50"
+        queries["apiKey"] = API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+
+        return queries
+    }
+
+    private fun setupRecyclerView(){
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
     }
     /**
     private fun loadDataFromCache() {
@@ -33,18 +86,20 @@ class RecipesFragment : Fragment() {
             })
         }
     }
-
+     */
     private fun showShimmerEffect(){
         binding.shimmerFrameLayout.startShimmer()
+        binding.shimmerFrameLayout.visibility = View.VISIBLE
         binding.recyclerview.visibility = View.GONE
     }
     private fun hideShimmerEffect() {
-    binding.shimmerFrameLayout.stopShimmer()
-    binding.recyclerview.visibility = View.VISIBLE
+        binding.shimmerFrameLayout.stopShimmer()
+        binding.shimmerFrameLayout.visibility = View.GONE
+        binding.recyclerview.visibility = View.VISIBLE
     }
      override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
      }
-    */
+
 }
